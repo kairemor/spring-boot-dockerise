fc14b7f529b5ba622ae5db1a099f617aab4d8490
kmtc44 <kairmor@yahoo.com>
2021-05-21 18:21:19 +0000

    nginx in php dockerfile mongo db running

--- src/main/java/com/sonatel/devops/cloud/clouddevops/CloudDevopsApplication.java
+++ src/main/java/com/sonatel/devops/cloud/clouddevops/CloudDevopsApplication.java
@@ -2,6 +2,10 @@ package com.sonatel.devops.cloud.clouddevops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CloudDevopsApplication {
@@ -10,4 +14,15 @@ public class CloudDevopsApplication {
 		SpringApplication.run(CloudDevopsApplication.class, args);
 	}
 
+}
+
+
+@Configuration
+@EnableWebMvc
+class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
+	}
 }
